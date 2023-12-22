8b6b677eef06e9ec4343f0db62c9e8b5c65fa2ab
Asem-001 <117676536+Asem-001@users.noreply.github.com>
2023-12-22 20:28:37 +0300

    files separation

--- public/js/script.js
+++ public/js/script.js
@@ -1,110 +1,14 @@
-function pearsonCorrelation(x, y) {
-  if (x.length !== y.length) {
-    throw new Error('Arrays must have the same length for correlation calculation.');
-  }
-
-  const n = x.length;
-
-  // Calculate the mean of x and y
-  const meanX = x.reduce((sum, value) => sum + value, 0) / n;
-  const meanY = y.reduce((sum, value) => sum + value, 0) / n;
-
-  // Calculate the numerator and denominators
-  let numerator = 0;
-  let denominatorX = 0;
-  let denominatorY = 0;
-
-  for (let i = 0; i < n; i++) {
-    const diffX = x[i] - meanX;
-    const diffY = y[i] - meanY;
-
-    numerator += diffX * diffY;
-    denominatorX += diffX ** 2;
-    denominatorY += diffY ** 2;
-  }
-
-  // Calculate the correlation coefficient
-  const correlation = numerator / Math.sqrt(denominatorX * denominatorY);
-
-  return correlation;
-}
-
-let employeeFullName = [];
-let employeeSalaryData = [];
-let testName = [];
-let testSalaryData = [];
-let counter = 0;
-
-function calc(){
-  const arr1 = employeeSalaryData.slice(-5);
-  const arr2 = testSalaryData.slice(-5);
-  console.log(arr1);
-  // Ensure both arrays have the same length
-  if (arr1.length !== arr2.length) {
-    throw new Error('Arrays must have the same length for correlation calculation.');
-  }
-  // [1,2,3,45,6,7,8,9,10,11,12,13,14,15,1,2,3,4]
-
-  let correlation = pearsonCorrelation(arr1, arr2).toFixed(2)
-  document.getElementById('correlationValue').innerText = correlation
-  
-}
-
-function updateData() {
-  if (employeeSalaryData.length < 15) {
-    number = Math.floor(Math.random() * 10000) + 1;
-    employeeSalaryData.push(number);
-  } else {
-    employeeSalaryData.shift();
-    number = Math.floor(Math.random() * 10000) + 1;
-    employeeSalaryData.push(number);
-  }
-}
-
-function updateLabels() {
-  let currentDate = new Date();
-
-  let hours = currentDate.getHours();
-  let minutes = currentDate.getMinutes();
-  let seconds = currentDate.getSeconds();
-
-  let formattedTime = `${hours}:${minutes}:${seconds}`;
-
-  if (testName.length < 15) {
-    testName.push(formattedTime);
-  } else {
-    testName.shift();
-    testName.push(formattedTime);
-  }
-}
-
-function updateTestData() {
-  if (testSalaryData.length < 15) {
-    number = Math.floor(Math.random() * 10000) + 1;
-    testSalaryData.push(number);
-  } else {
-    testSalaryData.shift();
-    number = Math.floor(Math.random() * 10000) + 1;
-    testSalaryData.push(number);
-  }
-}
-
-function updateTestLabels() {
-  let currentDate = new Date();
-
-  let hours = currentDate.getHours();
-  let minutes = currentDate.getMinutes();
-  let seconds = currentDate.getSeconds();
-
-  let formattedTime = `${hours}:${minutes}:${seconds}`;
-
-  if (employeeFullName.length < 15) {
-    employeeFullName.push(formattedTime);
-  } else {
-    employeeFullName.shift();
-    employeeFullName.push(formattedTime);
-  }
-}
+import {
+  updateTestLabels,
+  updateTestData,
+  updateLabels,
+  updateData,
+  employeeFullName,
+  employeeSalaryData,
+  calc,
+  testName,
+  testSalaryData
+} from './DataHandling.js'
 
 
 async function drawChart() {
@@ -147,11 +51,6 @@ async function drawChart() {
     theChart.data.datasets[1].data = testSalaryData
     // Update the chart
     theChart.update();
-    
-  
-     
-    
-  
   
   }, 2300);
 }
