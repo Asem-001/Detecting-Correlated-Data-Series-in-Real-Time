fea451fe3583b299cf7605ac2f909be63e4190ca
Asem-001 <117676536+Asem-001@users.noreply.github.com>
2023-12-23 23:39:45 +0300

    testing the data base

--- controller/controller.js
+++ controller/controller.js
@@ -5,12 +5,12 @@ module.exports = {
   index: async (req, res) => {
     const [result] = await pool.query(`
     SELECT * FROM correlation_db.detectedcorr as det, correlation_db.correaltion_data as data
-    where det.DataID = data.corrID
+    where det.DataID = data.corrID LIMIT 1
     `); // write query here the arr "[result]" to retun only the data if only use "result" will return data + the data types.
-    console.log(result);
-    
+   let obj  = result[0];
     res.render("index", {
       title: "Home page",
+      data: obj
     });
   },
 
