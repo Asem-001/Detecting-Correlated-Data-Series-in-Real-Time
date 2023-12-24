69ca36530c77bd4909f6f6e5fc59cf5bc3ae1248
Aziz Fahad <afbinsaif@sm.imamu.edu.sa>
2023-12-25 01:00:25 +0300

    dialog added
    delete and edit dialog has been added

--- public/js/uiscript.js
+++ public/js/uiscript.js
@@ -125,3 +125,103 @@
    
   
   })();
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Function to show the dialog
+    function showDialog(title, userData = {}) {
+      // Set the title
+      document.querySelector('#dialog .dialog-title').textContent = title;
+  
+      // Populate form fields if userData is provided (for Edit)
+      document.getElementById('validationCustom01').value = userData.id || '';
+      document.getElementById('validationCustom02').value = userData.name || '';
+      document.getElementById('validationCustom03').value = userData.email || '';
+      document.getElementById('validationCustom04').value = userData.adminId || '';
+      document.getElementById('validationCustom05').value = userData.accountDate || '';
+  
+      // Show the dialog
+      document.getElementById('dialog').style.display = 'block';
+    }
+  
+    // Function to hide the dialog
+    function closeDialog() {
+      document.getElementById('dialog').style.display = 'none';
+    }
+  
+    // Attach event listener to Add User button
+    document.getElementById('addUserBtn').addEventListener('click', function() {
+      showDialog('Add New User');
+    });
+  
+    // Attach event listeners to Edit buttons
+    const editButtons = document.querySelectorAll('.bi-pen');
+    editButtons.forEach(btn => {
+      btn.addEventListener('click', function() {
+        const row = this.closest('tr');
+        const userData = {
+          id: row.cells[0].innerText,
+          name: row.cells[1].innerText,
+          email: row.cells[2].innerText,
+          adminId: row.cells[3].innerText,
+          accountDate: row.cells[4].innerText
+        };
+        showDialog('Edit User', userData);
+      });
+    });
+  
+    // Attach event listener to close button in the dialog
+    document.querySelector('#dialog .dialog-close-btn').addEventListener('click', closeDialog);
+  
+    // Handle form submission
+    document.querySelector('#dialog form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // Retrieve form data
+      const formData = {
+        id: document.getElementById('validationCustom01').value,
+        name: document.getElementById('validationCustom02').value,
+        email: document.getElementById('validationCustom03').value,
+        adminId: document.getElementById('validationCustom04').value,
+        accountDate: document.getElementById('validationCustom05').value
+      };
+      
+      // Here, add your logic to process formData, such as sending it to a server or updating the table
+      console.log('Form Data:', formData);
+  
+      closeDialog();
+    });
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+  // ... your existing functions for Add and Edit ...
+
+  // Function to show the delete confirmation dialog
+  function showDeleteDialog() {
+    document.getElementById('deleteDialog').style.display = 'block';
+  }
+
+  // Function to hide the delete confirmation dialog
+  function closeDeleteDialog() {
+    document.getElementById('deleteDialog').style.display = 'none';
+  }
+
+  // Attach event listeners to Delete buttons
+  const deleteButtons = document.querySelectorAll('.bi-trash');
+  deleteButtons.forEach(btn => {
+    btn.addEventListener('click', function() {
+      showDeleteDialog();
+      // Store the row to be deleted
+      const rowToDelete = this.closest('tr');
+      
+      // Attach a one-time event listener to the confirm delete button
+      document.getElementById('confirmDeleteBtn').onclick = function() {
+        // Logic to delete the user
+        rowToDelete.remove();
+        closeDeleteDialog();
+        alert('User deleted successfully');
+      };
+    });
+  });
+
+  // Attach event listener to close button in the delete dialog
+  document.querySelector('#deleteDialog .dialog-close-btn').addEventListener('click', closeDeleteDialog);
+});
